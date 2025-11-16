58a23c3e7f3ba1ee8eeb63ddd642e827cb4e18f2
joaquinorradre <orradre.146258@e.unavarra.es>
2025-11-16 19:21:47 +0100

    Fixed Link Error

--- api/api.py
+++ api/api.py
@@ -73,4 +73,4 @@ async def resize_endpoint(
 
 
 if __name__ == "__main__":
-    uvicorn.run("api.fastapi_main:app", host="0.0.0.0", port=8000, reload=True) # pragma: no cover
+    uvicorn.run("api.api:app", host="0.0.0.0", port=8000, reload=True) # pragma: no cover
